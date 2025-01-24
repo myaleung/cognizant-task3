d68d0d6cd9b96ec1cbd1373da05f63773dd15538
Amy Leung <leungmya@gmail.com>
2025-01-24 14:40:52 +0000

    refactor: break down execute method

--- Scenario2/PreventDuplicateCasePlugin/PreventDuplicateCasePlugin.cs
+++ Scenario2/PreventDuplicateCasePlugin/PreventDuplicateCasePlugin.cs
@@ -7,6 +7,7 @@ namespace PreventDuplicateCasePlugin
 {
     public class PreventDuplicateCasePlugin : IPlugin
     {
+        // The main entry point for the plugin. This method is called when the plugin is executed
         public void Execute(IServiceProvider serviceProvider)
         {
             if (serviceProvider == null)
@@ -14,6 +15,7 @@ namespace PreventDuplicateCasePlugin
                 throw new ArgumentNullException(nameof(serviceProvider));
             }
 
+            // Obtain the execution context from the service provider
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             // Check if the plugin is running on case creation
@@ -22,24 +24,23 @@ namespace PreventDuplicateCasePlugin
                 return;
             }
 
-            // Get the service factory and tracing service
-            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
-            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            // Get the organization service and tracing service.
+            IOrganizationService service = GetOrganizationService(serviceProvider, context.UserId);
+            ITracingService tracingService = GetTracingService(serviceProvider);
 
             try
             {
-                // Retrieve the Target Entity (Case)
+                // Retrieve the Target Entity (Case) from the input parameters
                 Entity caseEntity = (Entity)context.InputParameters["Target"];
                 if (caseEntity == null) 
                 { 
                     return; 
                 }
 
-                // Get Account (Customer)
+                // Get the customer (account or contact) associated with the case
                 EntityReference customerRef = caseEntity.Contains("customerid") ? (EntityReference)caseEntity["customerid"] : null;
 
-                // Only check for duplicate cases if customer is an Account
+                // Only check for duplicate cases if customer is an account
                 if (customerRef == null || customerRef.LogicalName != "account") 
                 { 
                     return; 
@@ -56,14 +57,29 @@ namespace PreventDuplicateCasePlugin
             }
             catch (InvalidPluginExecutionException ex)
             {
+                // Trace the exception and rethrow it
                 tracingService.Trace("An active case already exists for this account. System cannot create a new one before it is resolved. {0}", ex.ToString());
                 throw;
             }
             catch (Exception ex)
             {
+                // Trace the exception and throw a new InvalidPluginExecutionException
                 tracingService.Trace("An error occurred in PreventDuplicateCase. Please contact support: {0}", ex.ToString());
                 throw new InvalidPluginExecutionException($"An error occurred in PreventDuplicateCase. {ex.Message}");
             }
+        }
+
+        // Service helper method to retrieve the organization service for the specified user.
+        private IOrganizationService GetOrganizationService(IServiceProvider serviceProvider, Guid userId)
+        {
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            return serviceFactory.CreateOrganizationService(userId);
+        }
+
+        // Retrieve the tracing service.
+        private ITracingService GetTracingService(IServiceProvider serviceProvider)
+        {
+            return (ITracingService)serviceProvider.GetService(typeof(ITracingService));
         }
 
         // Check if specific accountId has any active cases open
@@ -83,6 +99,7 @@ namespace PreventDuplicateCasePlugin
                 TopCount = 1 // Only need to check if there is at least one active case
             };
 
+            // Execute the query and return the count of active cases.
             EntityCollection existingCases = service.RetrieveMultiple(query);
             return existingCases.Entities.Count;
         }
